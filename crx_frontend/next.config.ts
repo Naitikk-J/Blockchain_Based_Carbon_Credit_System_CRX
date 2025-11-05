3e06518d8fd23deec88c93f91ae34dcb3bbea948
naitikjainjbp <naitikjainjbp@gmail.com>
2025-11-05 08:43:32 +0000

    Changes before Firebase Studio auto-run

--- crx_frontend/next.config.ts
+++ crx_frontend/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  devIndicators: {
+    allowedDevOrigins: [
+      "3000-firebase-crx2ogit-1762331035579.cluster-cd3bsnf6r5bemwki2bxljme5as.cloudworkstations.dev",
+    ],
+  },
 };
 
 export default nextConfig;
